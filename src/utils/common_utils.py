6399657b916588a57af0162b4749bc111beb3912
jacristi <jacristi0428@gmail.com>
2022-01-31 16:38:44 -0500

    chore: doc strings + comments

--- src/utils/common_utils.py
+++ src/utils/common_utils.py
@@ -6,7 +6,8 @@ from src.utils.data_models import UserError
 
 
 def error_handler(func):
-    """ """
+    """ Wrapper for handling potential errors. """
+
     @wraps(func)
     def wrapper(*args, **kwargs):
         """ """
@@ -39,50 +40,46 @@ class Logger(logging.Logger):
     log_file_path = 'output.log'
 
     def setup(self, config, log_level=logging.INFO):
-        """ """
+        """ Setup the logger. """
+
         self.config = config
         self.log_level = log_level if log_level is not None else logging.INFO
         self.log_file_path = config.get('LOG_FILE_PATH', self.log_file_path)
         self._set_console_handler()
-        self._set_file_handler()
         self.setLevel(self.log_level)
 
     def _set_console_handler(self):
-        """ """
+        """ Set a basic console handler. """
+
         ch = logging.StreamHandler()
         ch.setLevel(self.log_level)
         ch.setFormatter(self.custom_formatter)
         return True
 
-    def _set_file_handler(self):
-        """ """
-        return True
 
 
 class Timer:
-    """ """
+    """ Tiemr contextmanager for timing the start/end/interval for a given block of code."""
+
     start = None
     end = None
     interval = None
 
     def __init__(self, logger, message):
-        """ """
         self.logger = logger
         self.message = message
 
     def __enter__(self):
-        """ """
         self.start = datetime.now()
         if self.logger is not None:
             self.logger.info(f"Started | {self.message} | at {self.start}")
         else:
             print(f"Started | {self.message} | at {self.start}")
 
-
     def __exit__(self, err_class, err_obj, traceback):
-        """ """
         self.end = datetime.now()
         self.interval = (self.end - self.start).total_seconds()
+
         if self.logger is not None:
             self.logger.info(f"Finished | {self.message} | after {self.interval} at {self.end}")
         else:
